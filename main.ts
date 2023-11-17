f36291103656847a67cb965e6fc77427213e49f9
Fabian Schuh <mail@xeroc.org>
2023-11-17 21:12:14 +0100

    feat: allow to load a document from relay.md api

--- main.ts
+++ main.ts
@@ -1,13 +1,24 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, RequestUrlParam, RequestUrlResponse, requestUrl, TFile } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
 interface MyPluginSettings {
-	mySetting: string;
+	base_uri: string;
+	api_key: string;
+	vault_base_folder: string
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+	base_uri: 'https://api.relay.md',
+	api_key: '00000000-0000-0000-0000-000000000000',
+	vault_base_folder: "relay.md"
+}
+
+class Document {
+	id: string;
+	constructor(id: string) {
+		this.id = id;
+	}
 }
 
 export default class MyPlugin extends Plugin {
@@ -16,66 +27,18 @@ export default class MyPlugin extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
+		// Let's create a relay.md folder already
+		// TODO: maybe make this one configurable!
+		try {await vault.createFolder(this.settings.vault_base_folder);} catch(e) {}
+
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
+		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', async (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
+			await this.load_document("2b7ce5ca-dedd-40ae-abb4-ad58539a892c");
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
@@ -89,6 +52,55 @@ export default class MyPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+	async load_document(id: string) {
+		const vault = this.app.vault;
+		const options: RequestUrlParam = {
+			url: this.settings.base_uri + '/v1/doc/' + id,
+			method: 'GET',
+			headers: {
+				'X-API-KEY': this.settings.api_key,
+				'Content-Type': 'text/markdown'
+			},
+		}
+		var response: RequestUrlResponse;
+		try {
+			response = await requestUrl(options);
+			let filename: string = response.headers["x-relay-filename"];
+			let relay_to: dict = JSON.parse(response.headers["x-relay-to"]);
+			let body : string = response.text;
+
+			// Loop through team/topics
+			for (let to of relay_to) {
+				let tos: Array<string> = to.split("@", 2);
+				let team: string = tos[1];
+				let topic: string = tos[0];
+				let full_path_to_file: string;
+				// We ignore the "_" team which is a "global" team
+				if (team == "_") {
+					try {await vault.createFolder(this.settings.vault_base_folder + "/" + topic);} catch(e) {}
+					full_path_to_file = this.settings.vault_base_folder + "/" + topic + '/' + filename;
+				} else {
+					try {await vault.createFolder(this.settings.vault_base_folder + "/" + team);} catch(e) {}
+					try {await vault.createFolder(this.settings.vault_base_folder + "/" + team + "/" + topic);} catch(e) {}
+					full_path_to_file = this.settings.vault_base_folder + "/" + team + '/' + topic + '/' + filename;
+				}
+				let fileRef : TFile = vault.getAbstractFileByPath(full_path_to_file);
+				if(fileRef === undefined || fileRef === null) {
+					await vault.create(full_path_to_file, body);
+					new Notice('File ' + full_path_to_file + ' has been created!');
+				} else {
+					// TODO: consider storing multiple versions of a file here!
+					await vault.modify(fileRef, body);
+					new Notice('File ' + full_path_to_file + ' has been modified!');
+				}
+			}
+		} catch(e) {
+			console.log(JSON.stringify(e));
+			throw e;
+		}
+	}
+
 }
 
 class SampleModal extends Modal {
@@ -121,13 +133,35 @@ class SampleSettingTab extends PluginSettingTab {
 		containerEl.empty();
 
 		new Setting(containerEl)
+			.setName('Base API URI')
+			.setDesc('Base URL for API access')
+			.addText(text => text
+				.setPlaceholder('Enter your API url')
+				.setValue(this.plugin.settings.base_uri)
+				.onChange(async (value) => {
+					this.plugin.settings.base_uri = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
 			.setName('Setting #1')
 			.setDesc('It\'s a secret')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('Enter your API-Key')
+				.setValue(this.plugin.settings.api_key)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.api_key = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Vault relay.md inbox')
+			.setDesc('Base folder to synchronize into')
+			.addText(text => text
+				.setPlaceholder('relay.md')
+				.setValue(this.plugin.settings.vault_base_folder)
+				.onChange(async (value) => {
+					this.plugin.settings.vault_base_folder = value;
 					await this.plugin.saveSettings();
 				}));
 	}
