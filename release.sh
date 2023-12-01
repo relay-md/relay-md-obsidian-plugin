e4e117fa5c98b73cd46340aa94c60f324c0bd8fb
Fabian Schuh <mail@xeroc.org>
2023-12-01 11:53:28 +0100

    fix(build): use our release.sh instead of version-bump-mjs

--- release.sh
+++ release.sh
@@ -7,9 +7,15 @@ if [[ -z $1 ]]; then
 	exit 1
 fi
 
+# Store version number in versions.json
 TEMPFILE=$(mktemp)
 cp versions.json $TEMPFILE
 jq '."'$1'" = "'$REQUIRED_OBSIDIAN_VERSION'"' $TEMPFILE >versions.json
+
+# Store version number in manifest.json
+TEMPFILE=$(mktemp)
+cp manifest.json $TEMPFILE
+jq '.version = "'$1'"' $TEMPFILE >manifest.json
 
 # Now lets zip the files
 zip dist/release.zip main.js manifest.json styles.css
